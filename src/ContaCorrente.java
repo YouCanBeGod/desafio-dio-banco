2dc25f7e0402463bc45cb2a7f65f83ce60f262a3
YouCanBeGod <dallasta.amos@gmail.com>
2022-11-25 00:49:45 -0300

    Criaçao das principais classes e abstração inicial

---
+++ src/ContaCorrente.java
@@ -0,0 +1,18 @@
+public class ContaCorrente {
+
+    public  int agencia;
+    public  int conta;
+    public  int saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,18 @@
+public class ContaPoupanca {
+
+    public  int agencia;
+    public  int conta;
+    public int saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+}
