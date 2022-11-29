9d40737d18c901b7bce4d2a3ef24566b9b6aed99
YouCanBeGod <dallasta.amos@gmail.com>
2022-11-28 21:56:58 -0300

    Introdução a Herança

---
+++ src/Conta.java
@@ -0,0 +1,33 @@
+public abstract class Conta implements IConta{
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+
+    }
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+}
